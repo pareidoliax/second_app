cb48fad5cb1065c2e95f5075aea44fa351b339bf
epascual2014 <Edrickpascual@yahoo.com>
2014-02-18 12:11:14 -0800

    Installed Faker and seeded the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,15 @@
+require 'faker'
+
+rand(10..30).times do
+  p = Post.create(title: Faker::Lorem.words(rand(1..10)).join(" "), body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
+  rand(3..10).times do
+    p.comments.create(body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"))
+  end
+end
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
