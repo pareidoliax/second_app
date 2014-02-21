fceb8ddc3bb523eeb8a393ae1e1d0bcfab1ef0d1
epascual2014 <Edrickpascual@yahoo.com>
2014-02-21 10:46:05 -0800

    Implemented Devise 2

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,8 +4,8 @@ if Rails.env.development?
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => "your Sendgrid username",
-    :password       => "your Sendgrid password",
+    :user_name      => "app22273498@heroku.com",
+    :password       => "rm1wtrsr",
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
