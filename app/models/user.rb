390a0737f01782a7ea5d7cf7ef5cae13471e8e66
epascual2014 <Edrickpascual@yahoo.com>
2014-03-06 23:05:50 -0800

    Added image to Post.(vr2)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable,
+         :omniauthable, :omniauth_providers => [:facebook]
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, 
-                  :role, :avatar, :comments
+                  :role, :avatar, :provider, :uid
   # attr_accessible :title, :body
   has_many :posts
 
