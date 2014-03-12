0502d775b70c72a223906c0c9b39fecd6aa35a99
epascual2014 <Edrickpascual@yahoo.com>
2014-03-12 10:13:27 -0700

    undo pundit install

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
-  include Pundit
   protect_from_forgery
 
-  rescue_from Pundit::NotAuthorizedError do |exception|
+  rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
 end
--- app/policies/post_policy.rb
+++
@@ -1,9 +0,0 @@
-class PostPolicy < ApplicationPolicy
-  def index?
-    true
-  end
-
-  def destroy?
-    user.present? && (record.user == user || user.role?(admin) || user.role?(moderator))
-  end
-end
